06020b1fa93e3e98ef7297534b4d660969e28add
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-18 20:49:50 -0300

    feat: Tarefa, TarefaDao, TarefaDatabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.room3.common.jvm)
+    implementation(libs.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/IsaqueZaack/com/github/todolist/data/Tarefa.kt
+++ app/src/main/java/IsaqueZaack/com/github/todolist/data/Tarefa.kt
@@ -4,10 +4,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tarefas")
-data class Terefa (
+data class Tarefa(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val titulo: String,
     val descricao: String,
-    val concluido: Boolean = false,
+    val concluida: Boolean = false,
     val dataCriacao: Long = System.currentTimeMillis()
 )
